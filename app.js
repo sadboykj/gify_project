500b5196b2da356a25e86e7fc7b0cf694bafe57e
KJ Wilson <kjwilson505@yahoo.com>
2018-09-04 10:49:33 -0700

    Added home.handlebars

--- app.js
+++ app.js
@@ -4,10 +4,23 @@ var app = express();
 // express handlebars
 var exphbs = require('express-handlebars');
 
-app.get('/hello-squirrel', function (req,res) {
-    //Look up user from the database
-    //Render the template to display the user's info
-    res.send('Hello Squirrel')
+app.get('/hello-gif', function (req,res) {
+    // Look up gif from the database
+    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
+    // Render the template to display the user's info
+    res.render('hello-gif', {gifUrl: gifUrl})
+    // res.send('Hello Gif')
+});
+
+app.get('/greetings/:name' , function(req, res) {
+    // Store name
+    var name = req.params.name;
+    //Render to display name
+    res.render('greetings', {name: name});
+});
+
+app.get('/', function (req, res) {
+    res.render('home')
 });
 
 app.listen(3000, function () {

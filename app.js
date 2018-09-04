86fe1d80367be405290a7aa7bab0024e86c57f87
KJ Wilson <kjwilson505@yahoo.com>
2018-09-04 10:10:07 -0700

    Update js files Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 var express = require('express');
 var app = express();
+// express handlebars
+var exphbs = require('express-handlebars');
 
 app.get('/hello-squirrel', function (req,res) {
     //Look up user from the database
@@ -11,3 +13,6 @@ app.get('/hello-squirrel', function (req,res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+app.engine('handlebars', exphbs({defaultLayout: 'main}'}));
+app.set('view engine', 'handlebars');

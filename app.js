cbb4427f8670c858b98488cd0af4aa74a96fd545
KJ Wilson <kjwilson505@yahoo.com>
2018-09-04 11:20:23 -0700

    Nothing works! But saving anyway

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ var app = express();
 // express handlebars
 var exphbs = require('express-handlebars');
 
+app.engine('handlebars', exphbs({defaultLayout: 'main}'}));
+app.set('view engine', 'handlebars');
+
 app.get('/hello-gif', function (req,res) {
     // Look up gif from the database
     var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
     // Render the template to display the user's info
     res.render('hello-gif', {gifUrl: gifUrl})
     // res.send('Hello Gif')
+});
+
+app.get('/', function (req, res) {
+    res.render('home')
 });
 
 app.get('/greetings/:name' , function(req, res) {
@@ -19,13 +26,8 @@ app.get('/greetings/:name' , function(req, res) {
     res.render('greetings', {name: name});
 });
 
-app.get('/', function (req, res) {
-    res.render('home')
-});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-app.engine('handlebars', exphbs({defaultLayout: 'main}'}));
-app.set('view engine', 'handlebars');

cfd110974afa9f48b8d0f5e73d47ebf306481a6a
KJ Wilson <kjwilson505@yahoo.com>
2018-09-03 23:38:43 -0700

    Hello Squirrel!

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// app.js
 var express = require('express');
 var app = express();
 
+app.get('/hello-squirrel', function (req,res) {
+    //Look up user from the database
+    //Render the template to display the user's info
+    res.send('Hello Squirrel')
+});
+
 app.listen(3000, function () {
-  console.log('Gif Search listening on port localhost:3000!');
+  console.log('Example app listening on port 3000!');
 });

4118929552745393da9f341371c10f551f8f6ab8
KJ Wilson <yourboykjdev@gmail.com>
2018-09-06 10:18:25 -0700

    Updated everything. Working to finish in two hours

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var app = express();
 var exphbs = require('express-handlebars');
 // require http module
 var http = require('http');
+// initialize giphy-api library
+var giphy = require('giphy-api')();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main}'}));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/hello-gif', function (req,res) {
@@ -18,32 +20,38 @@ app.get('/hello-gif', function (req,res) {
 });
 
 app.get('/', function (req, res) {
-    console.log(req.query.term)
-    var queryString = req.query.term;
-    // remove white spaces and restricted characters
-    var term = encodeURIComponent(queryString)
-    // put search term in giphy api search gify URL
-    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-
-    http.get(url, function(response) {
-        // set encoding of response to utf8
-        response.setEncoding('utf8');
-
-        var body = '';
-
-        response.on('data', function(d) {
-            // continously update stream with data from giphy
-            body += d;
-        });
-
-        response.on('end', function() {
-            // when data is fulled recieved parse into json
-            var parsed = JSON.parse(body);
-            // render the home template and pass the gif data in to the template
-            res.render('home', {gifs: parsed.data})
-        });
-    });
+    giphy.search(req.query.term, function (err, response) {
+        res.render('home', {gifs: response.data})
+    })
 });
+
+// app.get('/', function (req, res) {
+//     console.log(req.query.term)
+//     var queryString = req.query.term;
+//     // remove white spaces and restricted characters
+//     var term = encodeURIComponent(queryString)
+//     // put search term in giphy api search gify URL
+//     var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+//
+//     http.get(url, function(response) {
+//         // set encoding of response to utf8
+//         response.setEncoding('utf8');
+//
+//         var body = '';
+//
+//         response.on('data', function(d) {
+//             // continously update stream with data from giphy
+//             body += d;
+//         });
+//
+//         response.on('end', function() {
+//             // when data is fulled recieved parse into json
+//             var parsed = JSON.parse(body);
+//             // render the home template and pass the gif data in to the template
+//             res.render('home', {gifs: parsed.data})
+//         });
+//     });
+// });
 
 app.get('/greetings/:name' , function(req, res) {
     // Store name
@@ -51,7 +59,6 @@ app.get('/greetings/:name' , function(req, res) {
     //Render to display name
     res.render('greetings', {name: name});
 });
-
 
 
 app.listen(3000, function () {
